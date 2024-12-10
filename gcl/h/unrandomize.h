da2b10b1cc4697cf1d5031246f25654a0909eb6a
Camm Maguire <camm@transcendence.maguirefamily.org>
2024-12-10 17:49:37 -0500

    support for immediate fixnums on hppa

--- gcl/h/unrandomize.h
+++ gcl/h/unrandomize.h
@@ -62,17 +62,24 @@
 	exit(-1);
       }
     }
-#if defined(CSTACKMAX) && CSTACK_DIRECTION < 0
-    if ((void *)&argc > (void *)CSTACKMAX) {
-      if (mmap((void *)CSTACKMAX-(1L << PAGEWIDTH),(1L << PAGEWIDTH),
-	       PROT_READ|PROT_WRITE|PROT_EXEC,MAP_FIXED|MAP_PRIVATE|MAP_ANON|MAP_STACK|MAP_GROWSDOWN,-1,0)==(void *)-1) {
-	printf("cannot mmap new stack %d\n",errno);
-	exit(-1);
-      }
-#ifdef SET_STACK_POINTER
-      {void *p=(void *)CSTACKMAX-4*CSTACK_ALIGNMENT;asm volatile (SET_STACK_POINTER::"r" (p):"memory");}
+#if defined(CSTACKMAX)
+#if CSTACK_DIRECTION < 0
+#define CSTACK_OFFSET (1L<<PAGEWIDTH)
+#define MAP_GROWSDOWN_FLAG MAP_GROWSDOWN
 #else
-#error Cannot set stack pointer
+#define CSTACK_OFFSET (1L<<23)
+#define MAP_GROWSDOWN_FLAG 0
+#endif
+    if ((void *)&argc > (void *)CSTACKMAX) {
+      if (mmap((void *)CSTACKMAX-CSTACK_OFFSET,(1L << PAGEWIDTH),
+	       PROT_READ|PROT_WRITE|PROT_EXEC,MAP_FIXED|MAP_PRIVATE|MAP_ANON|MAP_STACK|MAP_GROWSDOWN_FLAG,-1,0)==(void *)-1) {
+	  printf("cannot mmap new stack %d\n",errno);
+	  exit(-1);
+	}
+#ifdef SET_STACK_POINTER
+      {void *p=(void *)CSTACKMAX+4*CSTACK_ALIGNMENT*CSTACK_DIRECTION;asm volatile (SET_STACK_POINTER::"r" (p):"memory");}
+#else
+#error SET_STACK_POINTER undefined
 #endif
     }
 #endif
