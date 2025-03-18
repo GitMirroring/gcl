55587d6878cd336e4ccd198bf5a0f79f25d8dcff
Camm Maguire <camm@transcendence.maguirefamily.org>
2025-03-18 17:29:16 -0400

    simplify msbrk.c

--- gcl/o/msbrk.c
+++ gcl/o/msbrk.c
@@ -20,41 +20,8 @@ msbrk_end(void) {
 #if !defined(DARWIN) && !defined(__CYGWIN__) && !defined(__MINGW32__) && !defined(__MINGW64__)/*FIXME*/
 
 static void *
-new_amap(void *v,ufixnum s) {
-  return mmap(v,s,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_PRIVATE|MAP_ANON|MAP_FIXED,-1,0);
-}
-
-#if defined(__gnu_hurd___) && defined(__i386__)
-#define H1 (void *)0x20000000
-#define HE (void *)0x28000000
-#else
-#define H1 NULL
-#define HE NULL
-#endif
-
-static int
-old_mprotect(void *v,ufixnum s) {
-  void *ve=v+s;
-  v=v<H1 ? H1 : v;
-  ve=ve>HE ? HE : ve;
-  return  v<ve ? mprotect(v,ve-v,PROT_READ|PROT_WRITE|PROT_EXEC) : 0;
-}
-
-static void *
 new_map(void *v,ufixnum s) {
-
-  if (old_mprotect(v,s))
-    return (void *)-1;
-  if (v+s<H1)
-    return new_amap(v,s);
-  if (v<H1 && new_amap(v,H1-v)!=v)
-    return (void *)-1;
-  if (v+s<HE)
-    return v;
-  if (v<HE && new_amap(HE,v+s-HE)!=HE)
-    return (void *)-1;
-  return v<HE ? v : new_amap(v,s);
-
+  return mmap(v,s,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_PRIVATE|MAP_ANON|MAP_FIXED,-1,0);
 }
 
 int
