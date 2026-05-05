00f8e0c720fe3e8db78761471671395f4f5f135a
Camm Maguire <camm@transcendence.maguirefamily.org>
2026-05-05 10:54:41 -0400

    mach64_aarch64 reloc implementation

--- gcl/h/mach64_aarch64_reloc.h
+++ gcl/h/mach64_aarch64_reloc.h
@@ -1,26 +1,37 @@
-#include <mach-o/x86_64/reloc.h>
+#include <mach-o/arm64/reloc.h>
 
-#define GOT_RELOC(ri) ri->r_type==X86_64_RELOC_GOT_LOAD||ri->r_type==X86_64_RELOC_GOT
+#define GOT_RELOC(ri) ri->r_type==ARM64_RELOC_GOT_LOAD_PAGE21||ri->r_type==ARM64_RELOC_GOT_LOAD_PAGEOFF12
 
-
-  case X86_64_RELOC_UNSIGNED:		// for absolute addresses
-
-     if (ri->r_extern || !ri->r_pcrel) 
-      store_val(q,~0L,ri->r_pcrel ? a-rel : a);
-
-    break; 
-  case X86_64_RELOC_GOT_LOAD:		// a MOVQ load of a GOT entry
-  case X86_64_RELOC_GOT:		// a MOVQ load of a GOT entry
-
+  case ARM64_RELOC_BRANCH26:
+    add_vals(q,MASK(26),((long)(ri->r_pcrel ? a-(ul)q : a))/4);
+    break;
+  case ARM64_RELOC_GOT_LOAD_PAGE21:
     got+=n1[ri->r_symbolnum].n_desc-1;
     *got=a;
     a=(ul)got;
-
-  case X86_64_RELOC_SIGNED:		// for signed 32-bit displacement
-  case X86_64_RELOC_BRANCH:		// a CALL/JMP instruction with 32-bit displacement
-
-     if (ri->r_extern || !ri->r_pcrel) 	   
-       store_val(q,MASK(32),(ri->r_pcrel ? a-((ul)q+4) : a)+(signed)(*q&MASK(32)));
-
+  case ARM64_RELOC_PAGE21:
+#define PG(x) ((x) & ~0xfff)
+    a=((long)(PG(a)-PG((ul)q))) / 0x1000;
+#undef PG
+    massert(!(((*q)>>29)&0x3));
+    massert(!(((*q)>>5)&0x8ffff));
+    store_val(q,MASK(2) << 29, (a & 0x3) << 29);
+    store_val(q,MASK(19) << 5, (a & 0x1ffffc) << 3);
     break;
-
+  case ARM64_RELOC_GOT_LOAD_PAGEOFF12:
+    got+=n1[ri->r_symbolnum].n_desc-1;
+    *got=a;
+    a=(ul)got;
+  case ARM64_RELOC_PAGEOFF12:
+    a&=0xfff;
+    if (((*q)>>29)&0x1)
+      a>>=(((*q)>>30)&0x3);
+    massert(!(((*q)>>10)&0xfff));
+    store_val(q,MASK(12) << 10, a << 10);
+    break;
+  case ARM64_RELOC_UNSIGNED:
+    if (ri->r_extern || !ri->r_pcrel) {
+      massert(!*q);
+      store_val(q,~0UL,ri->r_pcrel ? a-rel : a);
+    }
+    break;
